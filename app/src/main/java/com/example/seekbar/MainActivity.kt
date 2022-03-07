d8ae42ab4997a137898d966b1f87f0baefea0c2f
LeonCookie <leongdynia1105@gmail.com>
2022-03-07 15:14:43 +0100

    second seekbar

--- app/src/main/java/com/example/seekbar/MainActivity.kt
+++ app/src/main/java/com/example/seekbar/MainActivity.kt
@@ -11,22 +11,45 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         lateinit var Suwak: SeekBar
+        lateinit var Suwak2: SeekBar
         lateinit var mapa: ImageView
 
-        var l:Float
+        var a:Float
 
 
 
         mapa = findViewById<ImageView>(R.id.imageViewObrazek)
         Suwak = findViewById<SeekBar>(R.id.seekBar)
+        Suwak2 = findViewById<SeekBar>(R.id.seekBar2)
 
         Suwak.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int,fromUser: Boolean) {
 
                 //mapa.setRotation(Suwak.progress.toFloat())
 
-                l = (progress.toFloat()/10)
-                mapa.alpha= l
+                a = (progress.toFloat())
+                mapa.alpha= a
+                mapa.rotation= a.toFloat()
+
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+        Suwak2.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int,fromUser: Boolean) {
+
+                //mapa.setRotation(Suwak.progress.toFloat())
+
+                a = (progress.toFloat())
+
+                mapa.rotationY= a.toFloat()
+
 
             }
 
